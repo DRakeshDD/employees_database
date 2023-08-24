50e3afef325bd4ba878b70a0366976ec3ab1348a
Rakesh <rakeshchinni9985@gmail.com>
2023-08-24 20:58:50 +0000

    comits..

---
+++ src/main/java/com/example/employee/model/EmployeeRowMapper.java
@@ -0,0 +1,29 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.jdbc.core.RowMapper;
+ * import java.sql.ResultSet;
+ * import java.sql.SQLException;
+ * 
+ */
+
+// Write your code here
+package com.example.employee.model;
+
+import java.util.*;
+import com.example.employee.model.Employee;
+
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmployeeRowMapper implements RowMapper<Employee> {
+    public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Employee(
+            rs.getInt("EmployeeId"),
+            rs.getString("EmployeeName"),
+            rs.getString("email"),
+            rs.getString("department")
+        );
+    }
+}
